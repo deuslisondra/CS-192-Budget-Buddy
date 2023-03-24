39fcba7e1694d198ad0a9255f64231445f3ba3de
Dean Ramirez <dpramirez@up.edu.ph>
2023-03-24 10:48:39 +0800

    changed fragments to activities

--- app/src/main/java/com/example/budgetbuddy/Dashboard.kt
+++ app/src/main/java/com/example/budgetbuddy/Dashboard.kt
@@ -1,45 +1,32 @@
 package com.example.budgetbuddy
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class Dashboard : AppCompatActivity() {
 
-    lateinit var bottomNav : BottomNavigationView
+    private lateinit var btnTransactions: Button
+    private lateinit var btnWallets: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dashboard)
-        loadFragment(DashboardFragment())
-        bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.dashboard -> {
-                    loadFragment(DashboardFragment())
-                    true
-                }
-                R.id.transactions -> {
-                    loadFragment(TransactionsFragment())
-                    true
-                }
-                R.id.wallets -> {
-                    loadFragment(WalletsFragment())
-                    true
-                }
-                R.id.profile -> {
-                    loadFragment(ProfileFragment())
-                    true
-                }
 
-                else -> {throw IllegalStateException()}
-            }
+        val btnTransactions = findViewById<Button>(R.id.btnTransactions) as Button
+        btnTransactions.setOnClickListener {
+            val intentTransactions = Intent(this, Transactions::class.java)
+            startActivity(intentTransactions)
         }
-    }
-    private fun loadFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.container,fragment)
-        transaction.commit()
+
+        val btnWallets = findViewById<Button>(R.id.btnWallets) as Button
+        btnWallets.setOnClickListener {
+            val intentWallets = Intent(this, Wallets::class.java)
+            startActivity(intentWallets)
+        }
+
     }
 }
