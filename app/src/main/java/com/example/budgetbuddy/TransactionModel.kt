c2a2c4d5fe05b408f87d6ffd0297c09d1f62108a
Dean <dpramirez@up.edu.ph>
2023-05-12 15:40:08 +0800

    Update TransactionModel.kt

--- app/src/main/java/com/example/budgetbuddy/TransactionModel.kt
+++ app/src/main/java/com/example/budgetbuddy/TransactionModel.kt
@@ -2,7 +2,7 @@ package com.example.budgetbuddy
 
 data class TransactionModel(
     var addTransactionName: String? = null,
-    var addTransactionBalance: String? = null
-    var addTransactionType: String? = null
+    var addTransactionBalance: String? = null,
+    var addTransactionType: String? = null,
     var addTransactionWallet: String? = null
 )
