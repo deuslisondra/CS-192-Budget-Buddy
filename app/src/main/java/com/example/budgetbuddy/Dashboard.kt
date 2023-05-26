8f739627c077df3cdd190d84210df920100db248
Dean <dpramirez@up.edu.ph>
2023-05-26 12:54:19 +0800

    added balance display and fixed navigation buttons

--- app/src/main/java/com/example/budgetbuddy/Dashboard.kt
+++ app/src/main/java/com/example/budgetbuddy/Dashboard.kt
@@ -2,12 +2,24 @@ package com.example.budgetbuddy
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import org.w3c.dom.Text
+import java.lang.Float.parseFloat
 
 class Dashboard : AppCompatActivity() {
+
+    private lateinit var dbRef : DatabaseReference
+    private lateinit var dbList: ArrayList<Int>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,5 +48,36 @@ class Dashboard : AppCompatActivity() {
             val intentSettings = Intent(this, Settings::class.java)
             startActivity(intentSettings)
         }
+
+        val sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE)
+        val userName = sharedPreferences.getString("username", "")
+
+        val balanceDisplay = findViewById<TextView>(R.id.textBalance) as TextView
+
+
+        dbRef = FirebaseDatabase.getInstance().getReference("Users/$userName/Wallets")
+
+        dbRef.addValueEventListener(object: ValueEventListener {
+            override fun onCancelled(error: DatabaseError) {
+            }
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+                var totalCount = 0.00f
+
+                for (child in snapshot.children) {
+                    val x = child.child("addWalletAmt")
+                    val y = parseFloat(x.value as String)
+
+                    totalCount += y
+
+                    //Log.i("TAG", "$y")
+                }
+                totalCount.toString()
+                balanceDisplay.text = "$totalCount"
+                //Log.i("totalCount", "$totalCount")
+            }
+        })
+
     }
+
 }
